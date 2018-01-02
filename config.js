e435ec38155051f0c18c486538ddc8a7e63eb995
moonou <moonoulong@gmail.com>
2018-01-02 18:21:40 +0800

    完善拼写错误

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 4000,
   magicNumber: 2,
-  device: 'ios', // 配置目标设备，andriod为安卓设备，ios为苹果设备
+  device: 'ios', // 配置目标设备，android为安卓设备，ios为苹果设备
   url: 'http://169.254.254.39:8100' // ios设备WebDriverAgent地址
 }
